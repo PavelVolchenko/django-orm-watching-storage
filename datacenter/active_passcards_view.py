1eaf9be02c16c56ffcc7daad52362d8d85f42732
Pavel <2793553@gmail.com>
2023-10-16 18:18:43 +0300

    task08 done

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -6,8 +6,15 @@ from django.shortcuts import render
 def active_passcards_view(request):
     # Программируем здесь
 
+    # [print(note) for note in Visit.objects.all()]
+    [print(note) for note in Visit.objects.filter(leaved_at__isnull=True).all()]
+
     active_passcards = Passcard.objects.filter(is_active=True).all()
     context = {
         'active_passcards': active_passcards,  # люди с активными пропусками
     }
     return render(request, 'active_passcards.html', context)
+
+
+
+
