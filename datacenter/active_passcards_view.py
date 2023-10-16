7dae4bec3cbba60eb9a8e16192209d0a02188853
Pavel <2793553@gmail.com>
2023-10-16 19:23:09 +0300

    task10 done

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -1,13 +1,19 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-
+from django.utils.timezone import localtime
+import datetime
 
 def active_passcards_view(request):
     # Программируем здесь
 
-    # [print(note) for note in Visit.objects.all()]
-    [print(note) for note in Visit.objects.filter(leaved_at__isnull=True).all()]
+    active_visitor = Visit.objects.filter(leaved_at__isnull=True).first()
+    delta = localtime() - active_visitor.entered_at
+    delta = datetime.timedelta(seconds=int(delta.total_seconds()))
+
+    print(f"\nЗашёл в хранилище, время по Москве:\n{active_visitor.entered_at}")
+    print(f"Находится в хранилище:\n{delta}")
+
 
     active_passcards = Passcard.objects.filter(is_active=True).all()
     context = {
