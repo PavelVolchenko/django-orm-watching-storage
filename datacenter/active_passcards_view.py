2c8ebd9cc730d227be4e9c1e0c8141bc4031b9e8
Pavel <2793553@gmail.com>
2023-10-17 21:46:47 +0300

    update .gitognore

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -1,16 +1,8 @@
 from datacenter.models import Passcard
-from datacenter.models import Visit
 from django.shortcuts import render
 
 def active_passcards_view(request):
-
-    # active_visitors = Visit.objects.filter(leaved_at__isnull=True).all()
-    # for visitor in active_visitors:
-    #     print(f"\n        Сотрудник: {visitor.passcard.owner_name}")
-    #     print(f"Зашёл в хранилище: {visitor.entered_at.strftime('%H:%M:%S (МСК) %d.%m.%Y')}")
-    #     print(f" Время нахождения: {visitor.format_duration()}")
-
-    active_passcards = Passcard.objects.filter(is_active=True).all()
+    active_passcards = Passcard.objects.filter(is_active=True)
     context = {
         'active_passcards': active_passcards,
     }
