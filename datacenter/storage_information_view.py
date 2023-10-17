62b050283618626040db20c2cf39426a5fcd0a99
Pavel <2793553@gmail.com>
2023-10-17 21:46:47 +0300

    update .gitognore

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -10,9 +10,24 @@ def storage_information_view(request):
             'entered_at': visitor.entered_at,
             'duration': visitor.format_duration(),
         }
-        for visitor in Visit.objects.filter(leaved_at__isnull=True).all()
+        for visitor in Visit.objects.filter(leaved_at__isnull=True)
     ]
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
+
+test_time = 60  # minutes
+suspects = list()
+passcode_list = Passcard.objects.values_list('passcode')
+for passcode in passcode_list:
+    print(f"{passcode=}")
+    pk = Passcard.objects.filter(passcode=passcode)
+    print(f"{pk=}")
+    visit_list = Visit.objects.filter(passcard=pk).all()
+    for visit in visit_list:
+        print(visit)
+        # if int(visit.get_duration()) >= test_time:
+        #     suspects.append(passcode)
+
+print(suspects)
